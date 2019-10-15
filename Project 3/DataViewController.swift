b88ad2a916557c1217cde58f389a1aeed3ab727a
Dylan Telson <dtelson1@binghamton.edu>
2019-10-14 22:35:15 -0400

    Started Project 3 with a Pageview Controller.

--- Project 3/DataViewController.swift
+++ Project 3/DataViewController.swift
@@ -16,7 +16,6 @@ class DataViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool) {
