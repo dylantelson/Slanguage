5256eecda70eb1f40e52b64f717ca65f47ef1efc
Dylan Telson <dtelson1@binghamton.edu>
2019-10-18 11:46:22 -0400

    Got the basics working: now the user is given a random phrase from an array, then must arrange the translation in the correct order by clicking on them, and then pressing "Check." If they are correct, it prints, "Correct!" and gives a new random phrase, while if incorrect it prints, "Incorrect!" and the user must see what they did wrong and rearrange the letters. Next step is making duplicates not happen, at least not consecutively, and adding more phrases.

--- Slanguage/Translate.swift
+++ Slanguage/Translate.swift
@@ -11,6 +11,8 @@ import UIKit
 var slangsArgOrig = ["Estoy al horno", "Tengo mala leche", "Sos un boludo"]
 var slangsArgTranslated = ["I'm screwed", "I have bad luck", "you're a jerk"]
 
+var userTranslation = [String]()
+
 var wordsToTranslate = [String]()
 var wordsToClick = [UIButton]()
 
@@ -30,6 +32,10 @@ class Translate: UIViewController {
     func newPhrase() {
         currString = Int.random(in: 0 ..< slangsArgOrig.count)
         textToTranslate.text = slangsArgOrig[currString]
+        for button in wordsToClick {
+            button.removeFromSuperview()
+        }
+        userTranslation.removeAll()
         wordsToTranslate.removeAll()
         wordsToClick.removeAll()
         wordsToTranslate = slangsArgTranslated[currString].components(separatedBy: " ")
@@ -40,17 +46,43 @@ class Translate: UIViewController {
             } else {
                 wordsToClick.append(UIButton(frame: CGRect(x: wordsToClick[n-1].frame.origin.x + wordsToClick[n-1].frame.width + 15, y: 500, width: 0, height: 0)))
             }
-            wordsToClick.last!.titleLabel!.text = wordsToTranslate[n]
+            wordsToClick.last!.setTitle(wordsToTranslate[n], for: .normal)
             wordsToClick.last!.frame = CGRect(x: wordsToClick.last!.frame.origin.x, y: wordsToClick.last!.frame.origin.y, width: wordsToClick.last!.titleLabel!.intrinsicContentSize.width + 10, height: wordsToClick.last!.titleLabel!.intrinsicContentSize.height + 4)
             wordsToClick.last!.backgroundColor = UIColor(red: 1, green: 0.478, blue: 0.478, alpha: 1)
             wordsToClick.last!.layer.cornerRadius = wordsToClick.last!.frame.height/2
-            wordsToClick.last!.setTitleColor(UIColor.white, for: .normal)
             wordsToClick.last!.layer.shadowColor = UIColor.darkGray.cgColor
             wordsToClick.last!.layer.shadowRadius = 4
             wordsToClick.last!.layer.shadowOpacity = 0.5
             wordsToClick.last!.layer.shadowOffset = CGSize(width: 0, height: 0)
             wordsToClick.last!.titleLabel!.textAlignment = .center
+            wordsToClick.last!.tag = n
+            wordsToClick.last!.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+            //wordsToClick.last!.titleLabel!.font = UIFont(name: "Helvetica", size: 19.0)
+            wordsToClick.last!.setTitleColor(UIColor.white, for: .normal)
             self.view.addSubview(wordsToClick.last!)
+        }
+    }
+    
+    @objc func buttonClicked(sender: UIButton!) {
+        print("Pressed!")
+        if(sender.frame.origin.y == 500) {
+            sender.frame.origin.y = 300
+            userTranslation.append(sender.titleLabel!.text!)
+        } else if(sender.frame.origin.y == 300) {
+            sender.frame.origin.y = 500
+            userTranslation.remove(at: userTranslation.firstIndex(of: sender.titleLabel!.text!)!)
+        }
+    }
+    
+    @IBAction func checkButtonClicked(sender: UIButton!) {
+        let finalTranslation = userTranslation.joined(separator: " ")
+        if(finalTranslation == slangsArgTranslated[currString]) {
+            print("Correct!")
+            newPhrase()
+        } else {
+            print("Given: " + finalTranslation)
+            print("Expected: " + slangsArgTranslated[currString])
+            print("Incorrect, please try again!")
         }
     }
     
