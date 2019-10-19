c429c8bed91e5920465bc257d509432658602e3d
Dylan Telson <dtelson1@binghamton.edu>
2019-10-19 00:11:20 -0400

    Worked on making the buttons change position correctly when others are clicked- now, for example, if the first (topleft-most) button is clicked, all the others on the row will move to the left to the correct position and those on the next row will also move accordingly (leftmost one move up a row and to the right, others left, etc.) Added random words, so now there are words shown that are not used in the phrase and the user must sift through and only select the correct ones (in the correct order). Also added more phrases.

--- Slanguage/Translate.swift
+++ Slanguage/Translate.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-var slangsArgOrig = ["Estoy al horno", "Tengo mala leche", "Sos un boludo"]
-var slangsArgTranslated = ["I'm screwed", "I have bad luck", "you're a jerk"]
+var randomEnglishWords = ["very", "super", "tired", "terrible", "jerk", "screwed", "but", "cheese", "I'm", "have", "a", "an", "really"]
+
+var slangsArgOrig = ["Estoy al horno", "Tengo mala leche", "Sos un boludo", "Che, tengo mucha fiaca", "Estoy re al pedo", "Dale, ponete las pilas", "Estoy en pedo", "Ni a palos", ]
+var slangsArgTranslated = ["I'm screwed", "I have bad luck", "you're a jerk", "dude, I'm really lazy", "I'm not doing anything", "c'mon, get yourself together", "I am drunk", "not even close"]
 
 var userTranslation = [String]()
 
 var wordsToTranslate = [String]()
 var wordsToClick = [UIButton]()
+
+var buttonsAtBottom = [UIButton]()
+var buttonsAtTop = [UIButton]()
+
 
 var currLanguage = "Arg"
 var currString = 0
@@ -38,13 +44,26 @@ class Translate: UIViewController {
         userTranslation.removeAll()
         wordsToTranslate.removeAll()
         wordsToClick.removeAll()
+        buttonsAtTop.removeAll()
+        buttonsAtBottom.removeAll()
         wordsToTranslate = slangsArgTranslated[currString].components(separatedBy: " ")
+        while(wordsToTranslate.count < 8) {
+            var randomWord = randomEnglishWords[Int.random(in: 0 ... randomEnglishWords.count - 1)]
+            if(wordsToTranslate.contains(randomWord)) {
+                continue
+            }
+            wordsToTranslate.append(randomWord)
+        }
         wordsToTranslate.shuffle()
         for n in 0 ..< wordsToTranslate.count {
             if(n == 0) {
                 wordsToClick.append(UIButton(frame: CGRect(x: 100, y: 500, width: 0, height: 0)))
-            } else {
+            } else if(n<4) {
                 wordsToClick.append(UIButton(frame: CGRect(x: wordsToClick[n-1].frame.origin.x + wordsToClick[n-1].frame.width + 15, y: 500, width: 0, height: 0)))
+            } else if(n==4){
+                wordsToClick.append(UIButton(frame: CGRect(x: 100, y: 600, width: 0, height: 0)))
+            } else {
+                wordsToClick.append(UIButton(frame: CGRect(x: wordsToClick[n-1].frame.origin.x + wordsToClick[n-1].frame.width + 15, y: 600, width: 0, height: 0)))
             }
             wordsToClick.last!.setTitle(wordsToTranslate[n], for: .normal)
             wordsToClick.last!.frame = CGRect(x: wordsToClick.last!.frame.origin.x, y: wordsToClick.last!.frame.origin.y, width: wordsToClick.last!.titleLabel!.intrinsicContentSize.width + 10, height: wordsToClick.last!.titleLabel!.intrinsicContentSize.height + 4)
@@ -59,17 +78,113 @@ class Translate: UIViewController {
             wordsToClick.last!.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
             //wordsToClick.last!.titleLabel!.font = UIFont(name: "Helvetica", size: 19.0)
             wordsToClick.last!.setTitleColor(UIColor.white, for: .normal)
+//            for word in wordsToClick {
+//                buttonsAtBottom.append(word)
+//            }
+            buttonsAtBottom = wordsToClick
             self.view.addSubview(wordsToClick.last!)
         }
     }
     
     @objc func buttonClicked(sender: UIButton!) {
-        print("Pressed!")
-        if(sender.frame.origin.y == 500) {
-            sender.frame.origin.y = 300
+        if(sender.frame.origin.y >= 500) {
+            let ind = buttonsAtBottom.firstIndex(of: sender)!
+            if(ind < buttonsAtBottom.count - 1) {
+                buttonsAtBottom[ind+1].frame.origin.x = buttonsAtBottom[ind].frame.origin.x + buttonsAtBottom[ind].frame.width + 15
+                buttonsAtBottom.remove(at: ind)
+            } else {
+                buttonsAtBottom.remove(at: ind)
+            }
+            for n in buttonsAtBottom.distance(from: buttonsAtBottom.startIndex, to: ind) ..< buttonsAtBottom.count {
+                print(n)
+                if(n == 3) {
+                    buttonsAtBottom[3].frame.origin.x = buttonsAtBottom[2].frame.origin.x + buttonsAtBottom[2].frame.width + 15
+                    buttonsAtBottom[3].frame.origin.y = 500
+                }
+                if(n == 4) {
+                    buttonsAtBottom[4].frame.origin.x = 100
+                } else if(n == 5) {
+                    //buttonsAtBottom[4].frame.origin.x = 100
+                    buttonsAtBottom[5].frame.origin.x = buttonsAtBottom[4].frame.origin.x + buttonsAtBottom[4].frame.width + 15
+                    if(buttonsAtBottom.count > 6) {
+                        buttonsAtBottom[6].frame.origin.x = buttonsAtBottom[5].frame.origin.x + buttonsAtBottom[5].frame.width + 15
+                        continue
+                    }
+                }
+                else if(n > 0) {
+                    buttonsAtBottom[n].frame.origin.x = buttonsAtBottom[n-1].frame.origin.x + buttonsAtBottom[n-1].frame.width + 15
+                } else {
+                    buttonsAtBottom[n].frame.origin.x = 100
+                }
+            }
+            //buttonsAtBottom.remove(at: buttonsAtBottom.firstIndex(of: sender)!)
+            if(userTranslation.count == 0 || buttonsAtTop.count == 4) {
+                sender.frame.origin.x = 100
+            } else {
+                sender.frame.origin.x = buttonsAtTop.last!.frame.origin.x + buttonsAtTop.last!.frame.width + 15
+            }
+            if(buttonsAtTop.count < 4) {
+                sender.frame.origin.y = 300
+            } else {
+                sender.frame.origin.y = 400
+            }
+            buttonsAtTop.append(sender)
             userTranslation.append(sender.titleLabel!.text!)
-        } else if(sender.frame.origin.y == 300) {
-            sender.frame.origin.y = 500
+        } else if(sender.frame.origin.y <= 400) {
+            let ind = buttonsAtTop.firstIndex(of: sender)!
+            if(ind < buttonsAtTop.count - 1) {
+                buttonsAtTop[ind+1].frame.origin.x = buttonsAtTop[ind].frame.origin.x + buttonsAtTop[ind].frame.width + 15
+                buttonsAtTop.remove(at: ind)
+            } else {
+                buttonsAtTop.remove(at: ind)
+            }
+            for n in buttonsAtTop.distance(from: buttonsAtTop.startIndex, to: ind) ..< buttonsAtTop.count {
+                print(n)
+                if(n == 3) {
+                    buttonsAtTop[3].frame.origin.x = buttonsAtTop[2].frame.origin.x + buttonsAtTop[2].frame.width + 15
+                    buttonsAtTop[3].frame.origin.y = 300
+                }
+                if(n == 4) {
+                    buttonsAtTop[4].frame.origin.x = 100
+                } else if(n == 5) {
+                    //buttonsAtBottom[4].frame.origin.x = 100
+                    buttonsAtTop[5].frame.origin.x = buttonsAtTop[4].frame.origin.x + buttonsAtTop[4].frame.width + 15
+                    if(buttonsAtTop.count > 6) {
+                        buttonsAtTop[6].frame.origin.x = buttonsAtTop[5].frame.origin.x + buttonsAtTop[5].frame.width + 15
+                        continue
+                    }
+                }
+                else if(n > 0) {
+                    buttonsAtTop[n].frame.origin.x = buttonsAtTop[n-1].frame.origin.x + buttonsAtTop[n-1].frame.width + 15
+                } else {
+                    buttonsAtTop[n].frame.origin.x = 100
+                }
+            }
+            //buttonsAtTop.remove(at: buttonsAtTop.firstIndex(of: sender)!)
+            if(buttonsAtBottom.count == 0 || buttonsAtBottom.count == 4) {
+                sender.frame.origin.x = 100
+            } else {
+                sender.frame.origin.x = buttonsAtBottom.last!.frame.origin.x + buttonsAtBottom.last!.frame.width + 15
+            }
+            if(buttonsAtBottom.count < 4) {
+                sender.frame.origin.y = 500
+            } else {
+                sender.frame.origin.y = 600
+            }
+            buttonsAtBottom.append(sender)
+//            var rightMostIndex = 0
+//            var rightMostXValue = 0
+//            for n in 0 ..< wordsToClick.count {
+//                if(wordsToClick[n].frame.origin.y == 500 && rightMostXValue < Int(wordsToClick[n].frame.origin.x)) {
+//                    rightMostXValue = Int(wordsToClick[n].frame.origin.x)
+//                    rightMostIndex = n
+//                }
+//            }
+//            if(rightMostXValue == 9999) {
+//                sender.frame.origin.x = 100
+//            } else {
+//                sender.frame.origin.x = wordsToClick[rightMostIndex].frame.origin.x + wordsToClick[rightMostIndex].frame.width + 15
+//            }
             userTranslation.remove(at: userTranslation.firstIndex(of: sender.titleLabel!.text!)!)
         }
     }
